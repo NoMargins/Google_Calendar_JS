4c1777cbf9674866e31d9af3677480258afdb773
NoMargins <olenaryabenko@gmail.com>
2022-09-02 14:55:22 +0300

    check for time-slots updates

--- scripts/calendar/timescale.js
+++ scripts/calendar/timescale.js
@@ -1,6 +1,8 @@
 import { createNumbersArray } from '../common/createNumbersArray.js';
 
 export const renderTimescale = () => {
+	const getTimeScaleElem = document.querySelector('.calendar__time-scale');
+
 	// ф-ция должна генерировать разметку для боковой шкалы времени (24 часа)
 	// полученную разметку вставьте на страницу с помощью innerHTML в .calendar__time-scale
 	const result = createNumbersArray(0, 24)
@@ -26,5 +28,4 @@ export const renderTimescale = () => {
 		.join('');
 
 	getTimeScaleElem.innerHTML = result;
-	return getTimeScaleElem;
 };
