3bda96e22c5e4ceddb51bb935cab619704ad94fc
NoMargins <olenaryabenko@gmail.com>
2022-09-05 13:03:57 +0300

    json package added

--- scripts/events/createEvent.js
+++ scripts/events/createEvent.js
@@ -5,27 +5,54 @@ import { closeModal } from '../common/modal.js';
 
 const eventFormElem = document.querySelector('.event-form');
 const closeEventFormBtn = document.querySelector('.create-event__close-btn');
+const submitBtnElem = document.querySelector('.event-form__submit-btn');
 
 function clearEventForm() {
-  // ф-ция должна очистить поля формы от значений
+	// ф-ция должна очистить поля формы от значений
+	eventFormElem.textContent = '';
 }
 
 function onCloseEventForm() {
-  // здесь нужно закрыть модальное окно и очистить форму
+	// здесь нужно закрыть модальное окно и очистить форму
+	closeModal();
+	clearEventForm();
 }
 
 function onCreateEvent(event) {
-  // задача этой ф-ции только добавить новое событие в массив событий, что хранится в storage
-  // создавать или менять DOM элементы здесь не нужно. Этим займутся другие ф-ции
-  // при подтверждении формы нужно считать данные с формы
-  // с формы вы получите поля date, startTime, endTime, title, description
-  // на основе полей date, startTime, endTime нужно посчитать дату начала и окончания события
-  // date, startTime, endTime - строки. Вам нужно с помощью getDateTime из утилит посчитать start и end объекта события
-  // полученное событие добавляем в массив событий, что хранится в storage
-  // закрываем форму
-  // и запускаем перерисовку событий с помощью renderEvents
+	// задача этой ф-ции только добавить новое событие в массив событий, что хранится в storage
+	// создавать или менять DOM элементы здесь не нужно. Этим займутся другие ф-ции
+	// при подтверждении формы нужно считать данные с формы
+	// с формы вы получите поля date, startTime, endTime, title, description
+	// на основе полей date, startTime, endTime нужно посчитать дату начала и окончания события
+	// date, startTime, endTime - строки. Вам нужно с помощью getDateTime из утилит посчитать start и end объекта события
+	// полученное событие добавляем в массив событий, что хранится в storage
+	// закрываем форму
+	// и запускаем перерисовку событий с помощью renderEvents
+	const startDateEvent = getDateTime(
+		event.target.date.value,
+		event.target.startTime.value
+	);
+	const endDateEvent = getDateTime(
+		event.target.date.value,
+		event.target.endTime.value
+	);
+
+	setItem({
+		date: event.target.date.value,
+		startTime: startDateEvent,
+		endTime: endDateEvent,
+		title: event.target.title.value,
+		description: event.target.description.value,
+	});
+
+	// event.target.date.value;
+	// event.target.title.value;
+	// event.target.description.value;
+	onCloseEventForm();
+	renderEvents();
 }
 
 export function initEventForm() {
-  // подпишитесь на сабмит формы и на закрытие формы
+	// подпишитесь на сабмит формы и на закрытие формы
+	submitBtnElem.addEventListener('click', onCreateEvent);
 }
